93069213de5725685435d6f406bea3ac8903afb8
Miguel Salazar <migueljsalazar@icloud.com>
2015-10-20 19:25:45 -0400

    Artworks up! and setting up /show links on Deep Dive

--- app/models/artist.rb
+++
@@ -1,3 +0,0 @@
-class Artist < ActiveRecord::Base
-  has_many :artworks
-end
--- db/migrate/20151018174651_add_artist_to_artwork.rb
+++
@@ -1,5 +0,0 @@
-class AddArtistToArtwork < ActiveRecord::Migration
-  def change
-    add_reference :artworks, :artist, index: true, foreign_key: true
-  end
-end
