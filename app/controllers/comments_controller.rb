cbcbfdf114a587d570ed47daa59639939fe78cc5
Madelene Campos <madelenecampos@gmail.com>
2015-10-20 09:02:09 -0400

    upvote feature works. Fixing downvote

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -67,6 +67,11 @@ class CommentsController < ApplicationController
     redirect_to(comments_path)
   end
 
+  def downvote
+    @comment = Comment.find(params[:id])
+    @comment.votes.last.destroy
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
